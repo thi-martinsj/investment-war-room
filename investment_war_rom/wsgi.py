ccbc79b810643b1c02327c3b35626f3eeca978f7
Thiago Martins <thiagomartins03@hotmail.com>
2022-07-15 13:59:22 -0300

    [sc-13] Initialize project

---
+++ investment_war_rom/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for investment_war_rom project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'investment_war_rom.settings')
+
+application = get_wsgi_application()
