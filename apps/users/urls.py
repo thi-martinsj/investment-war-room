870874d46280c7c12b3d6d6431b2521e8dd3450c
Thiago Martins <thiagomartins03@hotmail.com>
2022-07-17 12:53:42 -0300

    [sc-33] Create login, registration and logout functionality

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("login", views.login, name="login"),
     path("", views.dashboard, name="dashboard"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("logout", views.logout, name="logout")
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 
 
 def login(request):
+    if check_login(request):
+        return redirect("dashboard")
     return render(request, 'users/login.html')
 
 def register(request):
+    if create_register(request):
+        return redirect("login")
     return render(request, 'users/register.html')
 
 def dashboard(request):
@@ -12,3 +18,64 @@ def dashboard(request):
         return render(request, 'users/dashboard.html')
 
     return redirect('login')
+
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
+
+def check_login(request):
+    """Check user's credentials"""
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        if User.objects.filter(email=email).exists():
+            username = User.objects.filter(email=email).values_list("username", flat=True).get()
+            user = auth.authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return True
+
+        messages.error(request, "Invalid email or password")
+
+    return False
+
+def create_register(request):
+    """Create a register"""
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 != password2:
+            print("senhas precisam ser iguais")
+            messages.error(request, "Passwords must be the same")
+            return False
+
+        if User.objects.filter(email=email).exists():
+            print("usuário já existe email")
+            messages.error(request, "User already exists")
+            return False
+
+        if User.objects.filter(username=username).exists():
+            print("usuário já existe username")
+            messages.error(request, "User already exists")
+            return False
+
+        user = User.objects.create_user(
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            password = password1
+        )
+        user.save()
+        messages.success(request, "Successfully registered")
+
+        return True
+    
+    return False
+        
