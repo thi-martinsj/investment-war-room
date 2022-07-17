f694af885455378edf516f13971ea94aa56cdcb1
Thiago Martins <thiagomartins03@hotmail.com>
2022-07-17 02:08:28 -0300

    [sc-23] Add Dashboard

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,11 @@
-import re
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def login(request):
     return render(request, 'usuarios/login.html')
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+
+    return redirect('login')
