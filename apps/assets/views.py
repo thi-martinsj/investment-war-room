cb1f9695e4525d48566c9e1880e47b3face450a2
Thiago Martins <thiagomartins03@hotmail.com>
2022-07-18 00:08:24 -0300

    [sc-30] Add stock market request

--- apps/assets/views.py
+++ apps/assets/views.py
@@ -1,8 +1,15 @@
+import logging
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.contrib import messages
+from requests import get
 
 from .models import Assets, AssetsConfig, AssetsValues
+
+
+logger = logging.getLogger(__name__)
 
 
 def assets(request):
@@ -130,3 +137,27 @@ def get_config_params(request):
         "frequency": frequency,
         "is_active": is_active
     }
+
+def get_all_companies():
+    try:
+        companies = get(
+            url="https://api-cotacao-b3.labdo.it/api/empresa"
+        )
+
+        return companies.json()
+    except Exception:
+        logger.exception("Something went wrong when trying to get all companies")
+        return []
+
+def get_price(ticker):
+    try:
+        prices = get(
+            url=f"https://api-cotacao-b3.labdo.it/api/cotacao/cd_acao/{ticker}"
+        )
+        
+        last_price = prices.json()[0]
+
+        return last_price
+    except Exception:
+        logger.exception(f"Something went wrong when trying to get price from {ticker}")
+        return {}
